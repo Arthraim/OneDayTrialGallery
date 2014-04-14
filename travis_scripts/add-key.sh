538632f8750bb29bf126354dfa8f2f8e32b2f171
Arthur Wang <arthraim@gmail.com>
2014-04-14 21:06:42 +0800

    one more profile path fix

--- travis_scripts/add-key.sh
+++ travis_scripts/add-key.sh
@@ -4,4 +4,4 @@ security import ./travis_scripts/certs/AppleWWDRCA.cer -k ~/Library/Keychains/io
 security import ./travis_scripts/certs/one_day_trial_adhoc.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./travis_scripts/certs/one_day_trial_adhoc.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp ./travis_scripts/profile/$PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+cp ./travis_scripts/profiles/$PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
