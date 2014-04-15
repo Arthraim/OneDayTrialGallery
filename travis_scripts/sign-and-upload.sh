ab322e29abe07c29daba8f2b4f76811a3b408145
Arthur Wang <arthraim@gmail.com>
2014-04-15 12:58:04 +0800

    deploy to testflight

--- travis_scripts/sign-and-upload.sh
+++ travis_scripts/sign-and-upload.sh
@@ -15,3 +15,13 @@ xcrun -log -sdk iphoneos PackageApplication "$OUTPUTDIR/$APP_NAME.app" -o "$OUTP
 
 ls build/Release-iphoneos
 
+RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
+RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER\nUploaded: $RELEASE_DATE"
+
+curl http://testflightapp.com/api/builds.json \
+  -F file="@$OUTPUTDIR/$APP_NAME.ipa" \
+  -F dsym="@$OUTPUTDIR/$APP_NAME.app.dSYM.zip" \
+  -F api_token="$TESTFLIGHT_API_TOKEN" \
+  -F team_token="$TESTFLIGHT_TEAM_TOKEN" \
+  -F distribution_lists='Internal' \
+  -F notes="$RELEASE_NOTES"
