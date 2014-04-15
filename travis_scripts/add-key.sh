dd0f460759a72d99af5cfebf2e73ec54c45b0c1e
Arthur Wang <arthraim@gmail.com>
2014-04-15 10:46:36 +0800

    new certs and profiles

--- travis_scripts/add-key.sh
+++ travis_scripts/add-key.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 security create-keychain -p travis ios-build.keychain
 security import ./travis_scripts/certs/AppleWWDRCA.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./travis_scripts/certs/one_day_trial_adhoc.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./travis_scripts/certs/one_day_trial_adhoc.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ./travis_scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./travis_scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./travis_scripts/profiles/$PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
