eda60a29dd7bdfbe4fe81c99032626031b8a3828
Arthur Wang <arthraim@gmail.com>
2014-04-19 17:57:05 +0800

    tryout

--- travis_scripts/add-key.sh
+++ travis_scripts/add-key.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 security create-keychain -p travis ios-build.keychain
+ls ~/Library/Keychains/
 security import ./travis_scripts/certs/AppleWWDRCA.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./travis_scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./travis_scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
