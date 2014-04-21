4d51e9e052ba0f7b97c7a0de145f8c6c2192b369
Arthur Wang <arthraim@gmail.com>
2014-04-21 23:33:08 +0800

    set default in wrong way -.-

--- travis_scripts/add-key.sh
+++ travis_scripts/add-key.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-#security create-keychain -p travis iosbuild.keychain
-#security unlock-keychain -p travis iosbuild.keychain
-#security set-keychain-settings -u iosbuild.keychain
+security create-keychain -p travis iosbuild.keychain
+security unlock-keychain -p travis iosbuild.keychain
+security set-keychain-settings -u iosbuild.keychain
+security default-keychain -s iosbuild.keychain
 ls ~/Library/Keychains/
-security import ./travis_scripts/certs/AppleWWDRCA.cer -k ~/Library/Keychains/login.keychain -T `which codesign`
-security import ./travis_scripts/certs/dist.cer -k ~/Library/Keychains/login.keychain -T `which codesign`
-security import ./travis_scripts/certs/dist.p12 -k ~/Library/Keychains/login.keychain -P $KEY_PASSWORD -T `which codesign`
+security add-certificates ./travis_scripts/certs/AppleWWDRCA.cer
+security add-certificates ./travis_scripts/certs/dist.cer
+security import ./travis_scripts/certs/AppleWWDRCA.cer -k ~/Library/Keychains/iosbuild.keychain -T `which codesign` -T `which xctool` -T `which xcrun`
+security import ./travis_scripts/certs/dist.cer -k ~/Library/Keychains/iosbuild.keychain -T `which codesign` -T `which xctool` -T `which xcrun`
+security import ./travis_scripts/certs/dist.p12 -k ~/Library/Keychains/iosbuild.keychain -P $KEY_PASSWORD -T `which codesign` -T `which xctool` -T `which xcrun`
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./travis_scripts/profiles/$PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+
