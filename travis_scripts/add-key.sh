0c0475fa0fbe3b653208a75a9c25c6dc0cab66a0
Arthur Wang <arthraim@gmail.com>
2014-04-21 10:52:25 +0800

    which the programs

--- travis_scripts/add-key.sh
+++ travis_scripts/add-key.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 security create-keychain -p travis ios-build.keychain
 ls ~/Library/Keychains/
-security import ./travis_scripts/certs/AppleWWDRCA.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./travis_scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./travis_scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ./travis_scripts/certs/AppleWWDRCA.cer -k ~/Library/Keychains/ios-build.keychain -T `which codesign` -T `which xctool` -T `which xcrun`
+security import ./travis_scripts/certs/dist.cer -k ~/Library/Keychains/ios-build.keychain -T `which codesign` -T `which xctool` -T `which xcrun`
+security import ./travis_scripts/certs/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T `which codesign` -T `which xctool` -T `which xcrun`
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./travis_scripts/profiles/$PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
