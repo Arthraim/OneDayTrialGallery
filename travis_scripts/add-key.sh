b44a5e0ac7e0f82ef2f302e4f3fc234e4e564875
Arthur Wang <arthraim@gmail.com>
2014-04-21 23:11:44 +0800

    default keychain

--- travis_scripts/add-key.sh
+++ travis_scripts/add-key.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-security create-keychain -p travis iosbuild.keychain
-security unlock-keychain -p travis iosbuild.keychain
+#security create-keychain -p travis iosbuild.keychain
+#security unlock-keychain -p travis iosbuild.keychain
+security default-keychain -s iosbuild.keychain
 security set-keychain-settings -u iosbuild.keychain
 ls ~/Library/Keychains/
-security add-certificates ./travis_scripts/certs/AppleWWDRCA.cer
-security add-certificates ./travis_scripts/certs/dist.cer
 security import ./travis_scripts/certs/AppleWWDRCA.cer -k ~/Library/Keychains/iosbuild.keychain -T `which codesign` -T `which xctool` -T `which xcrun`
 security import ./travis_scripts/certs/dist.cer -k ~/Library/Keychains/iosbuild.keychain -T `which codesign` -T `which xctool` -T `which xcrun`
 security import ./travis_scripts/certs/dist.p12 -k ~/Library/Keychains/iosbuild.keychain -P $KEY_PASSWORD -T `which codesign` -T `which xctool` -T `which xcrun`
--- travis_scripts/remove-key.sh
+++ travis_scripts/remove-key.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-security delete-keychain iosbuild.keychain
+#security delete-keychain iosbuild.keychain
 rm -f ~/Library/MobileDevice/Provisioning\ Profiles/$PROFILE_NAME.mobileprovision
+
