4c5a8fb636854ccefdb4dff8e8d6028f09b8fc2f
Arthur Wang <arthraim@gmail.com>
2014-04-21 23:17:39 +0800

    no new keychain

--- travis_scripts/add-key.sh
+++ travis_scripts/add-key.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 #security create-keychain -p travis iosbuild.keychain
 #security unlock-keychain -p travis iosbuild.keychain
-security default-keychain -s iosbuild.keychain
-security set-keychain-settings -u iosbuild.keychain
-ls ~/Library/Keychains/
-security import ./travis_scripts/certs/AppleWWDRCA.cer -k ~/Library/Keychains/iosbuild.keychain -T `which codesign` -T `which xctool` -T `which xcrun`
-security import ./travis_scripts/certs/dist.cer -k ~/Library/Keychains/iosbuild.keychain -T `which codesign` -T `which xctool` -T `which xcrun`
-security import ./travis_scripts/certs/dist.p12 -k ~/Library/Keychains/iosbuild.keychain -P $KEY_PASSWORD -T `which codesign` -T `which xctool` -T `which xcrun`
+#security set-keychain-settings -u iosbuild.keychain
+#ls ~/Library/Keychains/
+security import ./travis_scripts/certs/AppleWWDRCA.cer -T `which codesign` -T `which xctool` -T `which xcrun`
+security import ./travis_scripts/certs/dist.cer -T `which codesign` -T `which xctool` -T `which xcrun`
+security import ./travis_scripts/certs/dist.p12 -P $KEY_PASSWORD -T `which codesign` -T `which xctool` -T `which xcrun`
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./travis_scripts/profiles/$PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
