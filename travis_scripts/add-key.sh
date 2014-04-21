714c68460df531ac6290b3bdc6d597714dd02af1
Arthur Wang <arthraim@gmail.com>
2014-04-21 23:28:14 +0800

    trytrytry

--- travis_scripts/add-key.sh
+++ travis_scripts/add-key.sh
@@ -2,9 +2,9 @@
 #security create-keychain -p travis iosbuild.keychain
 #security unlock-keychain -p travis iosbuild.keychain
 #security set-keychain-settings -u iosbuild.keychain
-#ls ~/Library/Keychains/
-security import ./travis_scripts/certs/AppleWWDRCA.cer -T `which codesign` -T `which xctool` -T `which xcrun`
-security import ./travis_scripts/certs/dist.cer -T `which codesign` -T `which xctool` -T `which xcrun`
-security import ./travis_scripts/certs/dist.p12 -P $KEY_PASSWORD -T `which codesign` -T `which xctool` -T `which xcrun`
+ls ~/Library/Keychains/
+security import ./travis_scripts/certs/AppleWWDRCA.cer -k ~/Library/Keychains/login.keychain -T `which codesign`
+security import ./travis_scripts/certs/dist.cer -k ~/Library/Keychains/login.keychain -T `which codesign`
+security import ./travis_scripts/certs/dist.p12 -k ~/Library/Keychains/login.keychain -P $KEY_PASSWORD -T `which codesign`
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./travis_scripts/profiles/$PROFILE_NAME.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
